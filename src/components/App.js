6893f68a1c7016003e43a21a85455f0dd233d0e8
sulaeman <sulaeman@rasyid.com>
2019-06-28 21:45:20 +0700

    a

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 class  App extends React.Component {
-  OnSeacrhSubmit(term){
+   state = { images: [] };
+
+
+
+  async OnSeacrhSubmit(term){
     console.log(term);
+    const response  = await
      axios.get('https://api.unsplash.com/search/photos',{
        params :{query: term},
        headers: {
         Authorization : 'Client-ID 6e6ce98c8ebdb235f1507ebcb6770bbf2751b948a6127aca5b3d28a7e8720035'
        }
-     })
-     .then(response => {
-       console.log(response.data.results)
-     })
-     ;
+     });
+     console.log(response.data.results);
+     // set state image
+    //  this.setState({ images : response.data.results });
   }
 
   render(){
