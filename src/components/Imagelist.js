1837e85fca819972f0edc497d06d4bef67c6f29f
sulaeman <sulaeman@rasyid.com>
2019-07-01 13:49:46 +0700

    Image card

--- src/components/Imagelist.js
+++ src/components/Imagelist.js
@@ -1,13 +1,11 @@
 import React from 'react';
-
+import ImageCard from './ImageCard';
 const ImageList= props => {
     
-  const gambar =  props.images.map((image)=>{
-               console.log(image.urls);
-
-     return  <div key={image.id}>
-            <img src={image.urls.regular} /> 
-     </div>
+  // const gambar =  props.images.map(({id,urls , description})=>{
+    const gambar =  props.images.map(image =>{            
+     return <ImageCard key={image.id} image={image}/>;
+    //  return   <img key={id} alt={description} src={urls.regular} /> 
      
   });
   
---
+++ src/components/sampleCom.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ImageCard extends React.Component{
+    render(){
+        return(
+
+        );
+    }
+}
