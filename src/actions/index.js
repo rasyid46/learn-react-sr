4e75b29688013fbc14ade2c899c90aae0ac53d50
sulaeman <sulaeman@rasyid.com>
2019-07-23 21:47:26 +0700

    memorize

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,11 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
-
+import _ from 'lodash';
 export const fetchPosts = () => async dispatch => {
    const response = await jsonPlaceholder.get('/posts');
    dispatch({type: 'FETCH_POSTS', payload : response.data }) 
 };  
 
-export const fetchUser = (id) => async dispatch => {
  
-   const response = await jsonPlaceholder.get('/users/'+id);
-   dispatch({type: 'FETCH_USER', payload : response.data }) 
-};  
 
 
 export const fetchUserAll = () => async dispatch => {
@@ -17,3 +13,11 @@ export const fetchUserAll = () => async dispatch => {
    const response = await jsonPlaceholder.get('/users/');
    dispatch({type: 'FETCH_USERALL', payload : response.data }) 
 };  
+
+
+export const fetchUser = _.memoize(function(id){
+  return _.memoize(async function(dispatch){
+   const response = await jsonPlaceholder.get('/users/'+id);
+   dispatch({type: 'FETCH_USER', payload : response.data }); 
+   });
+});
