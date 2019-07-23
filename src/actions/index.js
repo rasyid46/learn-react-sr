35b83ac1bad7ee65705032105762a804afef3cb5
sulaeman <sulaeman@rasyid.com>
2019-07-23 21:48:15 +0700

    a

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,17 +4,11 @@ export const fetchPosts = () => async dispatch => {
    const response = await jsonPlaceholder.get('/posts');
    dispatch({type: 'FETCH_POSTS', payload : response.data }) 
 };  
-
- 
-
-
 export const fetchUserAll = () => async dispatch => {
  
    const response = await jsonPlaceholder.get('/users/');
    dispatch({type: 'FETCH_USERALL', payload : response.data }) 
 };  
-
-
 export const fetchUser = _.memoize(function(id){
   return _.memoize(async function(dispatch){
    const response = await jsonPlaceholder.get('/users/'+id);
